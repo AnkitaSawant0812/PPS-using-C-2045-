4cc950884844f81f6ad911ed531d2bb1f2a7dcba
AnkitaSawant0812 <ankitabsawant0812@gmail.com>
2025-07-07 23:12:22 +0530

    Add files via upload

---
+++ program to calculate average 3 numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int num1, num2, num3;
+    float average;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("Enter third number: ");
+    scanf("%d", &num3);
+    average = (num1 + num2 + num3) / 3;
+    printf("The average of %d, %d and %d is %.2f\n", num1, num2, num3, average);
+    return 0;
+}
